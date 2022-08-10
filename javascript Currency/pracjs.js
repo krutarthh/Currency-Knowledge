1f4fd03387609fdb9a5bbd572aa17692a4d8babe
Krutarth Patel <93881557+krutarthh@users.noreply.github.com>
2022-08-10 15:50:48 -0400

    Add files via upload

---
+++ javascript Currency/pracjs.js
@@ -0,0 +1,253 @@
+
+document.getElementById("demo").innerHTML="Common show us your currency knowlege!";
+
+function Car(){
+    alert("You dont need a hint, You've got this!!");
+
+}
+
+function getcountry() {
+    selectElement = document.querySelector('#country');
+    output1 = selectElement.value;
+    return output1;
+
+
+}
+function getcurrency(){
+    sele=document.querySelector('#currency');
+    output2= sele.value;
+    return output2;
+
+
+
+
+}
+function checkanswer(){
+   if(getcountry()=="Afghanistan"){
+       if(getcurrency()=="AFN"){
+           alert("Good Job!!!");
+       }
+       else{
+           alert("Try again");
+       }
+   }
+   if(getcountry()=="Australia"){
+    if(getcurrency()=="AUD"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Belize"){
+    if(getcurrency()=="BZD"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Bhutan"){
+    if(getcurrency()=="BTN"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Bolivia"){
+    if(getcurrency()=="BOB"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Chile"){
+    if(getcurrency()=="CLP"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="China"){
+    if(getcurrency()=="CNY"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Congo, Democratic Republic of the Congo"){
+    if(getcurrency()=="CDF"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Egypt"){
+    if(getcurrency()=="EGP"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Ghana"){
+    if(getcurrency()=="GHS"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Greece"){
+    if(getcurrency()=="GRD"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Hong Kong"){
+    if(getcurrency()=="HKD"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="India"){
+    if(getcurrency()=="INR"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Iran, Islamic Republic of"){
+    if(getcurrency()=="IRR"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Kazakhstan"){
+    if(getcurrency()=="KZT"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Liberia"){
+    if(getcurrency()=="LRD"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Lithuania"){
+    if(getcurrency()=="LTL"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Macao"){
+    if(getcurrency()=="MOP"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Malawi"){
+    if(getcurrency()=="MWK"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Nepal"){
+    if(getcurrency()=="NPR"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Niger"){
+    if(getcurrency()=="NGN"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Panama"){
+    if(getcurrency()=="PAB"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Paraguay"){
+    if(getcurrency()=="PEN"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Qatar"){
+    if(getcurrency()=="QAR"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Turkmenistan"){
+    if(getcurrency()=="TMT"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="United States"){
+    if(getcurrency()=="USD"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Yemen"){
+    if(getcurrency()=="YER"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+if(getcountry()=="Zambia"){
+    if(getcurrency()=="ZMK"){
+        alert("Good Job!!!");
+    }
+    else{
+        alert("Try again");
+    }
+}
+1
+
+
+}
